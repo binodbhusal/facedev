6c3bbc4a409e59f238d77e981588a6c04101e5f3
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-11 18:49:29 +0000

    install tailwindcss

---
+++ config/tailwind.config.js
@@ -0,0 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'deep-blue': '#010026',
+        blue: '#2CBCE9',
+        red: '#DC4492',
+        yellow: '#FDCC49',
+        grey: '#ededed',
+        dblack:'#131417',
+        lblack:'#252830',
+        'dark-grey': '#757575',
+        'opaque-black': 'rgba(0,0,0,0.35)',
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
