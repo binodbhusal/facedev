f1888dc098e8e4da39f397b604d63378d1a08e93
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-12 22:41:10 +0000

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable 
+
+         PROFILE_TITLE = [
+        'Fullstack Developer',
+        'Frondend Developer',
+        'Backend Developer',
+        'React Developer',
+        'Ruby on Rails Developer',
+        'Senior Ruby on Rails Developer',
+        'Data Scientist',
+        'Software Engineer',
+        'Java Developer',
+        'Python Developer',
+        'Devops Developer',
+        'Cloud Developer',
+        'Software Architect'
+].freeze
+         def name
+          "#{first_name} #{last_name}".strip
+         end
 end
