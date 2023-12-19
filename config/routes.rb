bd32b27f08e3e45bb7d821907fbbcf6452546de6
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-19 23:45:50 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :members, only:[:show]
+  get 'edit_description', to: 'members#edit_description', as:'edit_user_description'
+  patch 'update_description', to: 'members#update_description', as:'update_user_description'
+  
+  get 'edit_personal_details', to: 'members#edit_personal_details', as:'edit_user_personal_details'
+  patch 'update_personal_details', to: 'members#update_personal_details', as:'update_user_personal_details'
 end
