7fe74626bab0b77eb18a20bc865f7d39b4caf024
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-20 19:41:10 +0000

    Add work experience model

---
+++ app/models/work_experience.rb
@@ -0,0 +1,34 @@
+class WorkExperience < ApplicationRecord
+
+  EMPLOYMENT_TYPE = ['Full-time', 'Part-time', 'Freelance', 'Self-employed',
+   'Trainee', 'Internship']
+   LOCATION_TYPE = ['Oon-site','Hybrid', 'Remote']
+  belongs_to :user
+  validates :start_date, :company, :job_title, :location, presence:true
+  validates :employment_type, presence: true, inclusion: {in: EMPLOYMENT_TYPE, message:'is not a valid employment type'}
+  validates :location_type, presence: true, inclusion: {in: LOCATION_TYPE, message:'is not a valid location type'}
+
+  validate :work_experiences_last_date
+  validate :presence_of_end_date
+  validate :end_date_greater_than_start_date
+
+              def end_date_greater_than_start_date if: :currently_not_working_here?
+                return if end_date.nil?
+                if end_date < start_date
+                  errors.add(:end_date, 'must be greater than start date')
+                end
+              end
+              def currently_not_working_here?
+                !currently_working_here
+              end
+              def work_experiences_last_date
+                if end_date.present? && currently_working_here
+                  errors.add(:end_date, 'must be blank if you select currently working here')
+                end
+              end
+              def presence_of_end_date
+                if end_date.nil? && !currently_working_here
+                  errors.add(:end_date,'must be present if you are not selected currently working here')
+              end
+            end
+end
