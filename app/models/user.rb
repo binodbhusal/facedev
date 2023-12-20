89160b2c55de2534920a836d448d2aa9fcf5b01f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-20 19:43:44 +0000

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable 
-
+  has_many :work_experiences
          PROFILE_TITLE = [
         'Fullstack Developer',
         'Frondend Developer',
