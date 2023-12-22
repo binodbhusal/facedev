62a42d8b985ba037a2dc15c918912d939bd0e8a4
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-22 23:23:30 +0000

    update work experience model

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -2,7 +2,7 @@ class WorkExperience < ApplicationRecord
 
   EMPLOYMENT_TYPE = ['Full-time', 'Part-time', 'Freelance', 'Self-employed',
    'Trainee', 'Internship']
-   LOCATION_TYPE = ['Oon-site','Hybrid', 'Remote']
+   LOCATION_TYPE = ['On-site','Hybrid', 'Remote']
   belongs_to :user
   validates :start_date, :company, :job_title, :location, presence:true
   validates :employment_type, presence: true, inclusion: {in: EMPLOYMENT_TYPE, message:'is not a valid employment type'}
