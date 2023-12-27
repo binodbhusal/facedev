c9baad4911105857de19fbd9898e876b36cd78b5
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-27 23:33:37 +0000

    added create, update, destroy action to work experience controller

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -7,9 +7,32 @@ class WorkExperiencesController < ApplicationController
     end
 
     def edit ; end
-    def create; end
-    def update; end
-    def destroy; end
+    def create
+        @work_experience = current_user.work_experiences.new(work_experience_params)
+        respond_to do |format|
+            if @work_experience.save
+                format.turbo_stream { render turbo_stream: turbo_stream.append('work_experience_items', partial:'work_experiences/work_exp', locals:{work_experience:@work_experience}) }
+            else 
+                format.turbo_stream {render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals:{form_partial: 'work_experiences/form', modal_title:'Add New Work Experience'})}
+            end 
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @work_experience.update(work_experience_params)
+                format.turbo_stream { render turbo_stream: turbo_stream.replace("work_experience_item#{@work_experience.id}", partial:'work_experiences/work_exp', locals:{work_experience:@work_experience}) }
+            else 
+                format.turbo_stream {render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals:{form_partial: 'work_experiences/form', modal_title:'Update New Work Experience'})}
+            end 
+        end
+    end
+    def destroy
+    respond_to do |format|
+        @work_experience.destroy
+        format.turbo_stream{render turbo_stream: turbo_stream.remove("work_experience_item#{@work_experience.id}")}
+    end
+    end
 private 
 def set_work_experience
 @work_experience = current_user.work_experiences.find(params[:id])
