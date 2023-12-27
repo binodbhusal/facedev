e658a21c4e8ccaae59c48dd4edb3b96d22125844
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-27 23:35:22 +0000

    added methods to work experience model

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -31,4 +31,27 @@ class WorkExperience < ApplicationRecord
                   errors.add(:end_date,'must be present if you are not selected currently working here')
               end
             end
+            def company_with_employment_type
+              "#{company} (#{employment_type})".strip
+            end
+            def job_duration
+              months = 
+                    if end_date.present?
+                  fetch_months(end_date)
+                    else 
+                    fetch_months(Date.today)
+                    end
+                    result = months.divmod(12)
+                    duration = "#{result.first} #{result.first > 1 ? 'years' : 'year' }#{result.last} #{result.last > 1 ? 'months' : 'month' }"
+
+                    if currently_working_here 
+                    "#{start_date.strftime("%b %y")} - Present (#{duration})"
+                    else
+                      "#{start_date.strftime("%b %y")} - #{end_date.strftime("%b %y")}(#{duration})"
+
+                    end
+            end
+            def fetch_months(last_date)
+              ((last_date.year - start_date.year) * 12 + last_date.month - start_date.month - (last_date.day >= start_date.day ? 0 : 1)).round
+            end
 end
