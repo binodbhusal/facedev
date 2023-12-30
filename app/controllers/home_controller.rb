f2910ffb17a9fc6872ea32394c8aeae98237330c
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-30 15:40:09 +0000

    add authenticate skip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @q = User.ransack(params[:q])
     @users = @q.result.limit(16).order(:created_at)
