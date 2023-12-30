59135c3ca011f8c83147f1e4601c29215f406cc3
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-30 15:42:29 +0000

    refactor code

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -1,5 +1,4 @@
 class WorkExperiencesController < ApplicationController 
-    before_action :authenticate_user!
     before_action :set_work_experience, only: %i[edit update destroy]
 
     def new 
@@ -7,31 +6,57 @@ class WorkExperiencesController < ApplicationController
     end
 
     def edit ; end
+
     def create
         @work_experience = current_user.work_experiences.new(work_experience_params)
-        respond_to do |format|
+       
             if @work_experience.save
-                format.turbo_stream { render turbo_stream: turbo_stream.append('work_experience_items', partial:'work_experiences/work_exp', locals:{work_experience:@work_experience}) }
+                render_turbo_stream(
+                            'append',
+                            'work_experience_items',
+                            'work_experiences/work_exp',
+                            {work_experience:@work_experience}                        
+                            ) 
             else 
-                format.turbo_stream {render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals:{form_partial: 'work_experiences/form', modal_title:'Add New Work Experience'})}
-            end 
+                render_turbo_stream(
+                'replace',
+                'remote_modal',
+                'shared/turbo_modal',
+                { form_partial: 'work_experiences/form', 
+                  modal_title:'Add New Work Experience'
+                } 
+                ) 
+        
+                    end
+                end
+
+    def update
+
+            if @work_experience.update(work_experience_params)
+                render_turbo_stream(
+                            'replace',
+                            "work_experience_item#{@work_experience.id}",
+                            'work_experiences/work_exp',
+                            {work_experience:@work_experience}   
+                            ) 
+            else 
+                render_turbo_stream(
+                            'replace',
+                            'remote_modal',
+                            'shared/turbo_modal',
+                            {form_partial: 'work_experiences/form', 
+                             modal_title:'Update New Work Experience'} 
+                             ) 
         end
     end
 
-    def update
-        respond_to do |format|
-            if @work_experience.update(work_experience_params)
-                format.turbo_stream { render turbo_stream: turbo_stream.replace("work_experience_item#{@work_experience.id}", partial:'work_experiences/work_exp', locals:{work_experience:@work_experience}) }
-            else 
-                format.turbo_stream {render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_modal', locals:{form_partial: 'work_experiences/form', modal_title:'Update New Work Experience'})}
-            end 
-        end
-    end
     def destroy
-    respond_to do |format|
+
         @work_experience.destroy
-        format.turbo_stream{render turbo_stream: turbo_stream.remove("work_experience_item#{@work_experience.id}")}
-    end
+        render_turbo_stream(
+            'remove',
+             "work_experience_item#{@work_experience.id}" 
+             ) 
     end
 private 
 def set_work_experience
