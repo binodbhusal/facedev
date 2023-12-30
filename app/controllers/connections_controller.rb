00ad2ca9f5ae5b50eff89f9d23b4d1b3716720b0
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-30 15:40:54 +0000

    Refactor connection controller

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,19 +1,36 @@
 class ConnectionsController < ApplicationController
-    before_action :authenticate_user!
     def index 
         @requested_connections = Connection.includes(:requested).where(user_id: current_user.id)
         @received_connections = Connection.includes(:received).where(connected_user_id: current_user.id)
     end
 
     def update 
-      @connection = Connection.find(params[:id]) 
-      respond_to do |format| 
-        if @connection.update(connection_params)
-            format.turbo_stream{ render turbo_stream: turbo_stream.replace("connection-status-#{@connection.id}", partial: 'connections/update', locals: {connection: @connection}) }
+        @connection = Connection.find(params[:id]) 
+        
+        respond_to do |format| 
+          ActiveRecord::Base.transaction do 
+            if @connection.update(connection_params)
+              if @connection.status =='accepted'
+                receiver = @connection.received
+                receiver.connected_user_ids << @connection.requested.id
+                receiver.save
+      
+                requester = @connection.requested
+                requester.connected_user_ids << @connection.received.id
+                requester.save
+              end
+            end
+      
+            format.turbo_stream { 
+              render turbo_stream: turbo_stream.replace("connection-status-#{@connection.id}", partial: 'connections/update', locals: { connection: @connection }) 
+            }
+          rescue StandardError => e
+            format.turbo_stream { render turbo_stream: turbo_stream.replace("connection-status-#{@connection.id}", partial: 'connections/error', locals: { error_message: e.message }) }
+            raise ActiveRecord::Rollback
+          end 
         end
       end
-    end 
-
+      
     def create 
         @connection = current_user.connections.new(connection_params)
         @connector = User.find(connection_params[:connected_user_id])
